ed309a7eff6cec081bce5a33e3b0d5bb2ab1ac86
handesaurabh <handesaurabh2004@gmail.com>
2025-11-29 12:23:21 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,20 +7,20 @@ const Menu = ({ items }) => {
                 const { id, title, img, desc, price, category } = menuItem;
 
                 return (
-                    <article
-                        key={id}
-                        className="menu-item"
-                        data-test-id={`menu-item-${category.toLowerCase()}`}
-                    >
-                        <img src={img} alt={title} className="photo" />
-                        <div className="item-info">
-                            <header>
-                                <h4>{title}</h4>
-                                <h4 className="price">$ {price}</h4>
-                            </header>
-                            <p className="item-text">{desc}</p>
-                        </div>
-                    </article>
+                  <article 
+    key={id} 
+    className="menu-item" 
+    data-test-id={`menu-item-${category}-${id}`}
+>
+    <img src={img} alt={title} className="photo" />
+    <div className="item-info">
+        <header>
+            <h4>{title}</h4>
+            <h4 className="price">$ {price}</h4>
+        </header>
+        <p className="item-text">{desc}</p>
+    </div>
+</article>
                 );
             })}
         </div>
