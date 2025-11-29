146a238a2e11085dc80ab916f7fce67b08b8870a
handesaurabh <handesaurabh2004@gmail.com>
2025-11-29 12:30:31 +0530

    Remove default 'all' filter button from Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,22 +3,13 @@ import React from "react";
 const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button
-        id="filter-btn-0"
-        type="button"
-        className="filter-btn"
-        onClick={() => filterItems("all")}
-      >
-        all
-      </button>
-
       {categories.map((category, index) => (
         <button
-          id={`filter-btn-${index + 1}`}
           type="button"
-          className="filter-btn"
           key={index}
+          className="filter-btn"
           onClick={() => filterItems(category)}
+          data-test-id={`filter-btn-${category}`} // Cypress selector
         >
           {category}
         </button>
