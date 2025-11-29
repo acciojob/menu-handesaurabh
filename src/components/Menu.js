15cac0f5acdf3b5d4400acb3defd5526e7b5a360
handesaurabh <handesaurabh2004@gmail.com>
2025-11-29 12:16:07 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,30 +1,30 @@
 import React from "react"
 
-const Menu = ({items}) => {
-    return(
+const Menu = ({ items }) => {
+    return (
         <div className="section-center">
-            {
-                items.map((menuItem)=>{
+            {items.map((menuItem) => {
+                const { id, title, img, desc, price, category } = menuItem;
 
-                    const {id,title,img,desc,price, category} = menuItem;
-
-                    return(
-                        <article category={category} key={id} className="menu-item " data-test-id={`menu-item-${category}`} >
-                            <img src={img} alt={title} className="photo" />
-                            <div className="item-info">
-                                <header>
-                                    <h4>{title}</h4>
-                                    <h4 className="price">$ {price}</h4>
-                                    
-                                </header>
-                                <p className="item-text">{desc}</p>
-                            </div>
-                        </article>
-                    )
-                })
-            }
+                return (
+                    <article
+                        key={id}
+                        className="menu-item"
+                        data-test-id={`menu-item-${category.toLowerCase()}`}
+                    >
+                        <img src={img} alt={title} className="photo" />
+                        <div className="item-info">
+                            <header>
+                                <h4>{title}</h4>
+                                <h4 className="price">$ {price}</h4>
+                            </header>
+                            <p className="item-text">{desc}</p>
+                        </div>
+                    </article>
+                );
+            })}
         </div>
-    )
-}
+    );
+};
 
-export default Menu
+export default Menu;
