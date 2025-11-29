53f44e4fef0d0830a7c45ddf15adf41b64d05ccf
handesaurabh <handesaurabh2004@gmail.com>
2025-11-29 12:10:29 +0530

    Update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,24 +1,30 @@
-import React from "react"
+import React from "react";
 
-const Categories = ({categories,filterItems}) => {
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button
+        id="filter-btn-0"
+        type="button"
+        className="filter-btn"
+        onClick={() => filterItems("all")}
+      >
+        all
+      </button>
 
-    return(
-        <div className="btn-container">
-            {
-                categories.map((category,index)=>{
-                    return(
-                        <button id={`filter-btn-${index}`}
-                            type="button"
-                            className="filter-btn"
-                            key={index}
-                            onClick={()=>filterItems(category)}>
-                                {category}
-                        </button>
-                    )
-                })
-            }    
-        </div>
-    )
-}
+      {categories.map((category, index) => (
+        <button
+          id={`filter-btn-${index + 1}`}
+          type="button"
+          className="filter-btn"
+          key={index}
+          onClick={() => filterItems(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
